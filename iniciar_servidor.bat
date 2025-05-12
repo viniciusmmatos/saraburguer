bfd66948cbe5fb54762b9d1f389ebd99251e9f24
viniciusmmatos <119702230+viniciusmmatos@users.noreply.github.com>
2025-05-12 19:00:20 -0300

    Update iniciar_servidor.bat
    Alterado script .bat para exibir o IP para acesso local.

--- iniciar_servidor.bat
+++ iniciar_servidor.bat
@@ -1,3 +1,14 @@
 @echo off
+cls
+echo Iniciando o servidor...
+
+for /f "tokens=2 delims=:" %%f in ('ipconfig ^| findstr /c:"IPv4"') do set IP=%%f
+set IP=%IP:~1%
+
+echo.
+echo Acesse o sistema em: http://%IP%:3000
+echo.
+
 node server.js
+
 pause
